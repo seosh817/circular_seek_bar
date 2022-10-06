6e756d110b86183b886da828be89ef313299fc6f
seunghwan <seosh817@naver.com>
2022-10-06 21:34:34 +0900

    Delete setState in handleGesture method

--- lib/circular_seek_bar.dart
+++ lib/circular_seek_bar.dart
@@ -179,9 +179,7 @@ class _CircularSeekBarState extends State<CircularSeekBar> {
         : _angleToProgress(angle > 0 ? angle : angle + 360, widget.startAngle,
             widget.sweepAngle);
     if (progress >= widget.minProgress && progress <= widget.maxProgress) {
-      setState(() {
         _progress = progress;
-      });
     }
   }
 
