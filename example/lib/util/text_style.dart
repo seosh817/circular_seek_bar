24ea318fccd747e697dbef6d5a4578c0dc81d038
seunghwan <seosh817@naver.com>
2022-10-06 13:52:11 +0900

    refactor: dart code format

--- example/lib/util/text_style.dart
+++ example/lib/util/text_style.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 
-const TextStyle kNotoSansBold12 = TextStyle(fontFamily: 'NotoSans', fontWeight: FontWeight.w700, fontSize: 12);
-const TextStyle kNotoSansRegular12 = TextStyle(fontFamily: 'NotoSans', fontSize: 12);
-const TextStyle kNotoSansMedium12 = TextStyle(fontFamily: 'NotoSans', fontWeight: FontWeight.w500, fontSize: 12);
+const TextStyle kNotoSansBold12 = TextStyle(
+    fontFamily: 'NotoSans', fontWeight: FontWeight.w700, fontSize: 12);
+const TextStyle kNotoSansRegular12 =
+    TextStyle(fontFamily: 'NotoSans', fontSize: 12);
+const TextStyle kNotoSansMedium12 = TextStyle(
+    fontFamily: 'NotoSans', fontWeight: FontWeight.w500, fontSize: 12);
 
-const TextStyle kNotoSansBold14 = TextStyle(fontFamily: 'NotoSans', fontWeight: FontWeight.w700, fontSize: 14);
-const TextStyle kNotoSansRegular14 = TextStyle(fontFamily: 'NotoSans', fontSize: 14);
-const TextStyle kNotoSansMedium14 = TextStyle(fontFamily: 'NotoSans', fontWeight: FontWeight.w500, fontSize: 14);
+const TextStyle kNotoSansBold14 = TextStyle(
+    fontFamily: 'NotoSans', fontWeight: FontWeight.w700, fontSize: 14);
+const TextStyle kNotoSansRegular14 =
+    TextStyle(fontFamily: 'NotoSans', fontSize: 14);
+const TextStyle kNotoSansMedium14 = TextStyle(
+    fontFamily: 'NotoSans', fontWeight: FontWeight.w500, fontSize: 14);
 
-const TextStyle kNotoSansBold16 = TextStyle(fontFamily: 'NotoSans', fontWeight: FontWeight.w700, fontSize: 16);
-const TextStyle kNotoSansRegular16 = TextStyle(fontFamily: 'NotoSans', fontSize: 16);
-const TextStyle kNotoSansMedium16 = TextStyle(fontFamily: 'NotoSans', fontWeight: FontWeight.w500, fontSize: 16);
+const TextStyle kNotoSansBold16 = TextStyle(
+    fontFamily: 'NotoSans', fontWeight: FontWeight.w700, fontSize: 16);
+const TextStyle kNotoSansRegular16 =
+    TextStyle(fontFamily: 'NotoSans', fontSize: 16);
+const TextStyle kNotoSansMedium16 = TextStyle(
+    fontFamily: 'NotoSans', fontWeight: FontWeight.w500, fontSize: 16);
 
-const TextStyle kNotoSansBold24 = TextStyle(fontFamily: 'NotoSans', fontWeight: FontWeight.w700, fontSize: 24);
-const TextStyle kNotoSansRegular24 = TextStyle(fontFamily: 'NotoSans', fontSize: 24);
-const TextStyle kNotoSansMedium24 = TextStyle(fontFamily: 'NotoSans', fontWeight: FontWeight.w500, fontSize: 24);
+const TextStyle kNotoSansBold24 = TextStyle(
+    fontFamily: 'NotoSans', fontWeight: FontWeight.w700, fontSize: 24);
+const TextStyle kNotoSansRegular24 =
+    TextStyle(fontFamily: 'NotoSans', fontSize: 24);
+const TextStyle kNotoSansMedium24 = TextStyle(
+    fontFamily: 'NotoSans', fontWeight: FontWeight.w500, fontSize: 24);
